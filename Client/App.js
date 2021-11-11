18c67655a1d6a4594e2b6f14264b010bceba22c6
marceloleiton <marceloleiton1997@gmail.com>
2021-11-11 00:57:47 -0300

    primer commit

--- Client/App.js
+++ Client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bienvenidos a la aplicación de React-Native</Text>
       <StatusBar style="auto" />
     </View>
   );

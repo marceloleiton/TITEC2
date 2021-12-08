c75f792186a56c52c0754d9e792f262b30995d76
marceloleiton <marceloleiton1997@gmail.com>
2021-12-08 00:31:44 -0300

    mejor stack en vista form

--- cliente/src/navigation/Router.js
+++ cliente/src/navigation/Router.js
@@ -11,7 +11,7 @@ const Router = props => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name={'Home'} component={HomeTabNavigation} options={{headerShown: false}} />
-        <Stack.Screen name={'Form'} component={FormSolicitudScreen} options={{headerShown: true}} />
+        <Stack.Screen name={'Form'} component={FormSolicitudScreen} options={{headerStyle: { backgroundColor: '#1259F5'},headerTitleStyle: { color: 'white'},headerTitle: 'Inscripción a evento'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
